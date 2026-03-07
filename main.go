13edc698746525743259354d3b659f2ffacb0b10
Edward Yang <edwardpyang@gmail.com>
2026-03-07 12:43:04 -0500

    move server to main

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
 
-	"fodmap/cli"
+	"fodmap/server"
 )
 
 func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, nil)))
-	cli.Execute()
+
+	srv, err := server.New(context.Background(), server.Config{
+		Port:       8080,
+		PromptPath: "./prompt.txt",
+	})
+	if err != nil {
+		slog.Error("failed to initialize server", "error", err)
+		os.Exit(1)
+	}
+	if err := srv.Start(); err != nil {
+		slog.Error("server error", "error", err)
+		os.Exit(1)
+	}
 }
