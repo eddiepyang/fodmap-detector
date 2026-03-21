ff70f53f3af3a8947c3242aa146091af68665f17
Edward Yang <edwardpyang@gmail.com>
2026-03-21 16:26:57 -0400

    update response (#7)
    Co-authored-by: ryeyoo <ryeyoo@pop-os.lan>

--- main.go
+++ main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"context"
 	"log/slog"
 	"os"
 
-	"fodmap/server"
+	"fodmap/cli"
 )
 
 func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, nil)))
-
-	srv, err := server.New(context.Background(), server.Config{
-		Port:         8080,
-		PromptPath:   "./prompt.txt",
-		WeaviateHost: os.Getenv("WEAVIATE_HOST"),
-	})
-	if err != nil {
-		slog.Error("failed to initialize server", "error", err)
-		os.Exit(1)
-	}
-	if err := srv.Start(); err != nil {
-		slog.Error("server error", "error", err)
-		os.Exit(1)
-	}
+	cli.Execute()
 }
