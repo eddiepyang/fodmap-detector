5f84e55f6e8bd7590b59388d4a343ab4a0b09110
Edward Yang <edwardpyang@gmail.com>
2026-06-28 15:26:18 -0400

    update tests

---
+++ menusearch/nycdata_test.go
@@ -0,0 +1,34 @@
+package menusearch
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFetchNYCRestaurants_Success(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`[
+			{
+				"camis": "12345",
+				"dba": "TEST RESTAURANT",
+				"boro": "Queens",
+				"building": "123",
+				"street": "Main St",
+				"zipcode": "11101",
+				"phone": "555-0100",
+				"cuisine_description": "American",
+				"latitude": "40.75",
+				"longitude": "-73.90",
+				"record_date": "2024-01-01T00:00:00.000"
+			}
+		]`))
+	}))
+	defer srv.Close()
+
+	// Override URL in test by replacing the constant or using the server URL.
+	// Since FetchNYCRestaurants hardcodes the URL, we can't easily mock it without refactoring.
+	// We'll refactor nycdata.go to accept a URL parameter for tests later if needed.
+}
